80da3338cca9f365d50202f5994e851848710fcd
vinu679 <49819942+vinu679@users.noreply.github.com>
2022-07-29 10:04:50 +0530

    Update Task.py

--- Task.py
+++ Task.py
@@ -2,27 +2,17 @@ import spacy
 import spacy.displacy as displacy
 import re
 
-def udd_get_nationalities_Spacy(text):
-  ## Required Static Nationalities List
-  nationalities = {'british','scottish','irish','welsh','danish','finnish',
-                   'norwegian','swedish','swiss','estonian','latvian','lithuanian',
-                   'austrian','belgian','french','german','italian','dutch',
-                   'american','canadian','mexican','ukrainian','russian','belarusian',
-                   'polish','czech','slovak','slovakian','hungarian','romanian','bulgarian','greek','spanish'}
-
-  nlp = spacy.load("en_core_web_lg", disable=['parser', 'tagger', 'textcat', 'lemmatizer'])
-  text = text.strip()
-  text = re.sub("\s+", " ", text)
-
-  if len(text) > 1000000:
-    return []
-  else:
-    nlp.max_length = 1000000
-
+def udd_get_named_entities(text, keyword):
+  nlp = spacy.load("en_core_web_lg")
   doc = nlp(text)
-  result = list(set([ent.text.lower() for ent in doc.ents]).intersection(nationalities))
+  result = []
+  keyList = {'language': 'LANGUAGE', 'nationality': 'NORP', 'religion': 'NORP'}
+  for ent in doc.ents:
+    if (ent.label_ in keyList[keyword]) and ("".join(ent.text.split()).isalpha()):
+      result.append(ent.text)
+  return result
 
-  return result;
+udd_get_named_entities('This is an english language and I am american and I am an Hindu', 'language')
 
 
 https://gist.github.com/joshuabaker/d2775b5ada7d1601bcd7b31cb4081981
